f99eb041d6034f65cdada7a865662de833a3bff4
Vlad Eliseev <freshymanchan@gmail.com>
2020-06-21 18:17:23 +1000

    token fix

--- src/index.js
+++ src/index.js
@@ -10,5 +10,7 @@ import '@/assets/css/normalize.css';
 import '@/assets/css/reset.css';
 import '@/assets/css/base.css';
 
+localStorage.setItem('token', '9e6ba03e430b41914f8b6cd44b2ca9f567a7d04c');
+
 const container = document.querySelector('#TheApp');
 ReactDOM.render(<App />, container);
